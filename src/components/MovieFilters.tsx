2d5857c0eff4679f9797a607ea663b25f4d65b71
Oscar Beamish <oscartbeamish@gmail.com>
2025-08-01 15:38:42 +0100

    Unusued import removed

--- src/components/MovieFilters.tsx
+++ src/components/MovieFilters.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Genre } from '../types/Movie';
 import { movieService } from '../services/movieService';
-import SearchIcon from '@mui/icons-material/Search';
 import './MovieFilters.css';
 
 interface MovieFiltersProps {
